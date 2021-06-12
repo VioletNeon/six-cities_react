a61262a561f47854c97f568651d081ed3ba2f189
Valeriy <KulikovVR@gmail.com>
2021-06-12 18:25:09 +0300

    Начинаем программировать

--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  offerCount: 5,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      offerCount={Setting.offerCount}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
